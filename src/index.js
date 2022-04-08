5ee39a067428ffc3a4213ec710c8c0e20368f909
Mahdi Khavari <mkhavari813@gmail.com>
2022-04-08 15:22:29 +0430

    implementing Redux

--- src/index.js
+++ src/index.js
@@ -11,11 +11,16 @@
 import App from './App';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
 root.render(
-  <BrowserRouter>
-    <App tab="home" />
-  </BrowserRouter>
+  <Provider store={store} >
+    <BrowserRouter>
+      <App tab="home" />
+    </BrowserRouter>
+  </Provider>
 );
--- src/pages/website/Home.js
+++ src/pages/website/Home.js
@@ -1,7 +1,33 @@
-const Home = () => {
+import { useEffect } from "react"
+import { PropTypes } from "prop-types"
+import { connect } from "react-redux"
+import { fetchPosts } from "../../redux/actions/postActions"
+
+const HomeComponent = (props) => {
+  useEffect(()=>{
+    props.fetchPosts();
+  },[])
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      {props.posts.map((el,index)=>{
+        return <li key={el.id} >{index}</li>
+      })}
+    </>
   )
 }
 
-export {Home}
+HomeComponent.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
+
+// export {Home}
+
+// export default connect(null, { fetchPosts })(Home)
+const mapStateToProps = state => ({
+  posts : state.posts.items
+})
+
+const Home = connect(mapStateToProps, { fetchPosts })(HomeComponent)
+export { Home }
