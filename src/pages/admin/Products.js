4ec23bb2c0be3cfffdab2a27dfa3e23042b88ed3
Mahdi Khavari <mkhavari813@gmail.com>
2022-04-09 13:33:24 +0430

    add filtration to orders page + update action

--- src/pages/admin/Products.js
+++ src/pages/admin/Products.js
@@ -2,15 +2,26 @@ import { useSelector } from "react-redux";
 import { fetchProducts } from "../../redux/actions/productsActions";
 import { Pagination } from "../../components/Pagination";
 import { TableGrid } from "../../components/TableGrid";
+import Button from '@mui/material/Button';
+
 
 const Products = (props) => {
   const state = useSelector(state => state)
-  console.log(state)
   return (
     <>
       <section className="container mt-4" >
+        <div className="d-flex align-items-center justify-content-between">
+          <Button variant="contained" className="mb-4" color="success">
+            <span className=" vazir-medium" >
+              افزودن کالا 
+            </span>
+          </Button>
+          <h1 className="h1 vazir-bold text-end mb-5 text-primary p-2 rounded px-4">
+            مدیریت کالاها
+          </h1>
+        </div>
         <TableGrid headers={["",'گروه','دسته بندی','نام کالا','تصویر']} state={state.products} bodyItems={["group","headgroup","name",""]} />
-        <Pagination actionFunc={fetchProducts(1, 4)} pageNumbers={3} pageLimitation={3} />
+        <Pagination actionFunc={fetchProducts(1, 3)} pageNumbers={3} pageLimitation={3} />
       </section>
     </>
   )
--- src/redux/actions/ordersAction.js
+++ src/redux/actions/ordersAction.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 import { FETCH_ORDERS } from "./types";
 
-export const fetchOrders = (page, limit) => dispatch => {
-  axios.get(`${process.env.REACT_APP_BACKEND_URL}` + `/orders?_page=${page}&_limit=${limit}`).then((res) => {
-    console.log('res',res.data)
+export const fetchOrders = (page, limit,delivered) => dispatch => {
+  axios.get(`${process.env.REACT_APP_BACKEND_URL}` + `/orders?_page=${page}&_limit=${limit}&delivered=${delivered}`).then((res) => {
     dispatch({
       type: FETCH_ORDERS,
       payload: res.data
